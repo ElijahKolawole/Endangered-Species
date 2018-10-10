089ecb7af6618231b09ecec78d75b07b34ea3c9b
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-10-10 15:41:11 -0400

    added pics to grid view. Need to display in the miage view upon itemClick

--- app/src/main/java/com/adefemikolawole/endangeredspecies/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/endangeredspecies/MainActivity.java
@@ -1,18 +1,22 @@
 package com.adefemikolawole.endangeredspecies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import static android.widget.AdapterView.*;
 
 public class MainActivity extends AppCompatActivity {
     final String TAG = MainActivity.class.getSimpleName().toString();
-    Integer[] animals = {R.drawable.eagle, R.drawable.elephant, R.drawable.gorilla , R.drawable.panda, R.drawable.panther, R.drawable.polar};
+    Integer[] Animals = {R.drawable.eagle, R.drawable.elephant, R.drawable.gorilla , R.drawable.panda, R.drawable.panther, R.drawable.polar};
     ImageView pic;
 
 
@@ -25,31 +29,50 @@ public class MainActivity extends AppCompatActivity {
         GridView grid = findViewById(R.id.gridView);
         final ImageView pic = findViewById(R.id.imgLarge);
         grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Selected Species " + (position + 1), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
     public class ImageAdapter extends BaseAdapter {
-        public ImageAdapter(MainActivity mainActivity) {
+        private Context context;
+        public ImageAdapter(Context context) {
+             this.context = context ; // context is cusomized to hold context resources.
+
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return Animals.length;
         }
 
         @Override
         public Object getItem(int position) {
             return null;
+
         }
 
         @Override
         public long getItemId(int position) {
             return 0;
+
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            pic = new ImageView(context); //instance of image view
+            pic.setImageResource(Animals[position]);
+            pic.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic.setLayoutParams(new GridView.LayoutParams(330, 300));
+            return  pic;
+
+
         }
     }
 }
